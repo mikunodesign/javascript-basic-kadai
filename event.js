7112eedea267fdbcf89d7464b70899ab8e028640
mikunodesign <143928781+mikunodesign@users.noreply.github.com>
2023-09-04 17:39:31 +0900

    Add files via upload

---
+++ event.js
@@ -0,0 +1,7 @@
+const buttonElement =document.getElementById('btn');
+const h2Element = document.getElementById('text');
+
+buttonElement.addEventListener('click',  () => {
+   h2Element.textContent = 'ボタンをクリックしました';
+
+});
