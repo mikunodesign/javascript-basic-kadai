5a251b620876b9c36eea8cade6c185047b4857ac
mikunodesign <143928781+mikunodesign@users.noreply.github.com>
2023-09-04 18:14:20 +0900

    chapter20

--- event.js
+++ event.js
@@ -2,6 +2,9 @@ const buttonElement =document.getElementById('btn');
 const h2Element = document.getElementById('text');
 
 buttonElement.addEventListener('click',  () => {
-   h2Element.textContent = 'ボタンをクリックしました';
 
+   setTimeout(()=> {
+      h2Element.textContent = 'ボタンをクリックしました';
+   },2000);
+   
 });
